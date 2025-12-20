93f8eae9440b9ffa89d9ea711e143cc64695f38a
Slim-Beatnik <totem64@gmail.com>
2025-12-20 12:14:29 -0700

    Refactor Landing page to use NavLink for navigation and verify tailwindcss coloring works, update styles, and add dependenciesMeta for prettier plugin

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,22 +1,24 @@
+import { NavLink } from 'react-router';
+
 export default function Landing() {
   return (
     <section className="relative min-h-screen overflow-hidden">
       <div className="dark bg-dracula-bg min-h-screen">
-        <div className="container mx-auto px-4 py-20 sm:min-w-100 md:min-w-80">
-          <h1 className="text-dracula-foreground mb-6 text-center text-4xl font-bold sm:w-80 sm:text-5xl md:text-6xl lg:text-7xl">
+        <div className="container mx-auto justify-center px-4 py-20 sm:min-w-100 md:min-w-80">
+          <h1 className="text-dracula-foreground xs:w-full mx-auto mb-6 w-80 text-center text-4xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">
             Welcome to the Task App
           </h1>
-          <p className="text-dracula-foreground mb-8 text-justify text-lg">
+          <p className="text-dracula-fg mx-3 mb-8 text-justify text-lg">
             Manage your tasks efficiently and stay organized with our intuitive
             task management application.
           </p>
           <div className="flex justify-center">
-            <a
-              href="/tasks"
-              className="text-shadow-dracula-red bg-dracula-purple hover:text-dracula-red hover:text-shadow-dracula-foreground hover:bg-dracula-purple-shift w-fit rounded-lg px-6 py-3 font-semibold transition-colors duration-300"
+            <NavLink
+              to="/tasks"
+              className="text-shadow-dracula-red bg-dracula-pink hover:text-dracula-red hover:text-shadow-dracula-fg hover:bg-dracula-purple-shift border-dracula-pink-shift w-fit rounded-xl border-2 px-6 py-3 font-semibold transition-colors duration-300 hover:text-shadow-2xs"
             >
               Get Started
-            </a>
+            </NavLink>
           </div>
         </div>
       </div>
