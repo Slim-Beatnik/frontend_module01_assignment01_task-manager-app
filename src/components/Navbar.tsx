97f25b802a8baf190026e0b55879585ebe31e0f9
Slim-Beatnik <totem64@gmail.com>
2026-01-10 11:37:38 -0700

    Replace favicon with checkOff.svg for navbar brand and update Navbar logo size and layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { NavLink, useLocation } from 'react-router';
-import logo from '/favicon.svg?url';
+import logo from '/checkOff.svg?url';
 
 export default function Navbar() {
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
@@ -33,29 +33,27 @@ export default function Navbar() {
   return (
     <>
       {currPath !== '/callback' && (
-        <nav className="bg-dracula-bg flex flex-row items-center justify-between gap-24 px-4 py-2 shadow-md">
+        <nav className="bg-dracula-bg flex flex-row items-center justify-between gap-2 px-4 py-2 shadow-md md:gap-24">
           <div className="flex justify-start">
             <img
               src={logo}
               alt="Logo"
-              className="h-20 w-20"
+              className="h-20 w-38"
             />
-            <span className="text-stroke-fill-dracula-red text-stroke-dracula-selection text-stroke-3 ml-2 self-center text-7xl font-semibold whitespace-nowrap">
-              Check-off
-            </span>
           </div>
           <div className="grow" />
 
           {currPath !== '/' && (
             // Only render nav links on pages other than home
-            <div className="grid grid-cols-4 gap-4 md:flex md:gap-6">
+            <div className="grid grow grid-cols-4 gap-4 md:flex md:gap-6">
               {/* Render nav links dynamically -- to add page make entry in navTitlesPaths */}
 
               {Object.entries(navTitlesPaths).map(([title, path]) => (
                 <NavLink
+                  key={title}
                   to={path}
                   aria-disabled={currPath === path}
-                  className="aria-disabled:bg-dracula-comment aria-disabled:text-dracula-bg-light bg-dracula-orange text-dracula-bg hover:bg-dracula-orange-shift hover:text-dracula-bg-darker inline-block rounded-2xl px-4 py-2 font-bold aria-disabled:pointer-events-none aria-disabled:cursor-not-allowed"
+                  className="aria-disabled:bg-dracula-comment none aria-disabled:text-dracula-bg-light bg-dracula-orange text-dracula-bg hover:bg-dracula-orange-shift hover:text-dracula-bg-darker inline-block min-w-fit rounded-2xl px-4 py-2 text-sm font-bold aria-disabled:pointer-events-none aria-disabled:cursor-not-allowed md:text-lg"
                   onClick={(e) => noopOnDisable(e, path)}
                 >
                   {title}
