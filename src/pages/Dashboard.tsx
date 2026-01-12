3efbf8fc46df34638dea85c8d1192a05b21c108e
Slim-Beatnik <totem64@gmail.com>
2026-01-12 10:17:03 -0700

    added 2 column layout

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,12 @@
 function Dashboard() {
-  return <></>;
+  return (
+    <div className="p-12">
+      <div className="grid grid-cols-2">
+        <div className="bg-dracula-cyan-shift grid min-h-full min-w-full grid-rows-1"></div>
+        <div className="bg-dracula-purple-shift grid min-h-full min-w-full grid-rows-1"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
