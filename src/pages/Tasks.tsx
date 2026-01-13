37fb456e87dcdb8df7cb3a3a00473a77f4d1bf22
Slim-Beatnik <totem64@gmail.com>
2026-01-13 10:49:37 -0700

    Add Tasks.tsx based on previous idea for dashboard.

---
+++ src/pages/Tasks.tsx
@@ -0,0 +1,15 @@
+export default function AllTasks() {
+  return (
+    <div className="bg-dracula-comment h-[calc(100vh-6rem)] min-w-screen p-12">
+      <div className="grid min-h-full min-w-full grid-cols-2 gap-6">
+        <section className="h-full grid-rows-1">
+          <h2 className="bg-dracula-cyan-shift/50 text-dracula-bg-darker text-center">
+            Tasks On Today
+          </h2>
+          <div className="bg-dracula-cyan-shift grid h-full w-full"></div>
+        </section>
+        <div className="bg-dracula-purple-shift grid min-h-full min-w-full grid-rows-1"></div>
+      </div>
+    </div>
+  );
+}
